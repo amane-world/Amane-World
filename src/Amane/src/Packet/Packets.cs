623b7224dfa70a835aa8e078c7f0b93197db7864
sanaehururu1200 <sanaehururu1200@gmail.com>
2018-10-07 00:32:28 +0900

    JSON完全に理解してなかった

--- src/Amane/src/Packet/Packets.cs
+++ src/Amane/src/Packet/Packets.cs
@@ -2,6 +2,6 @@
 
 namespace Packet
 {
-  public class SayPacket : Packet { public SayPacket(string msg) : base(0x0E, $"{{msg: \"{msg}\"}}") { } }
+  public class SayPacket : Packet { public SayPacket(string msg) : base(0x0E, $"{{\"msg\": \"{msg}\"}}") { } }
   // class ChunkPacket : Packet { ChunkPacket() : base(0x22, "{}") { } }
 }
